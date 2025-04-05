aa7000641db97273848d9d213df55cef94bc50aa
Aaron Palacios Olea <apalacios.dev@gmail.com>
2025-04-05 16:20:18 +0200

    clean repo

--- src/lib/ngx-forms-validator.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-ngx-forms-validator',
-  template: ''
-})
-export class NgxFormsValidatorComponent {
-
-}
--- src/public-api.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/services/formValidator.service'
-export * from './lib/services/translate.service'
-export * from './lib/validators/custom-validator.component'
